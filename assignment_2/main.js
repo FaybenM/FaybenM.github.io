7da77f8bbee5e7b3dc2a818e8a2218dd87ed9be8
F <puppy02468@gmail.com>
2020-04-01 20:23:48 -0400

    10

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -6,26 +6,18 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-for(var i = 1; i <= 5; i++) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
-    thumbBar.appendChild(newImage);
-    newImage.onclick = function(e) {
-      displayedImage.src = e.target.src;
-    }
-  }
+
   
   /* Wiring up the Darken/Lighten button */
   
-  btn.onclick = function() {
-    const btnClass = btn.getAttribute('class');
-    if(btnClass === 'dark') {
-      btn.setAttribute('class','light');
-      btn.textContent = 'Lighten';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } else {
-      btn.setAttribute('class','dark');
-      btn.textContent = 'Darken';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  function Color(){
+    const classButton = btn.getAttribute('class');
+    btn.setAttribute("class", "light");
+    btn.innerText = "Lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    if(classButton === "light"){
+      btn.setAttribute("class","dark");
+      btn.innerText = "Darken";
+      overlay.style.backgroundColor = "rgba(0,0,0,0)";
     }
   }

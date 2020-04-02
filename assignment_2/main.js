42c3a51316f1f21b173c698d6951109d375967cb
F <puppy02468@gmail.com>
2020-04-02 16:38:47 -0400

    draft

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -3,10 +3,18 @@ const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
+//when working on the page we did base our code on the MDN IMage Gallery page and wanted to reference it
 
 /* Looping through images */
 
-
+for(var i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(e) {
+      displayedImage.src = e.target.src;
+    }
+  }
   
   /* Wiring up the Darken/Lighten button */
   

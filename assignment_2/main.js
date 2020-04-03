c2f1496833027e373a1719f7b599cb5b12877e90
F <puppy02468@gmail.com>
2020-04-03 13:43:19 -0400

    not new update
    previous ones that were comitted on time are the final ones; this is not any new work past deadline!

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -6,19 +6,25 @@ const overlay = document.querySelector('.overlay');
 //when working on the page we did base our code on the MDN IMage Gallery page and wanted to reference it
 
 /* Looping through images */
-
-
+for(var i = 1; i <= 5; i++) {
+  const addPic = document.createElement('img');
+  addPic.setAttribute('src', `images/pic${i}.jpg`);
+  thumbBar.appendChild(addPic);
+  addPic.onclick = function(l) {
+    displayedImage.src = l.target.src;
+  }
+}
   
   /* Wiring up the Darken/Lighten button */
   
-  function Color(){
-    const classButton = btn.getAttribute("class");
-    btn.setAttribute("class", "light");
-    btn.innerText = "Lighten";
-    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
-    if(classButton === "light"){
-      btn.setAttribute("class","dark");
-      btn.innerText = "Darken";
-      overlay.style.backgroundColor = "rgba(0,0,0,0)";
-    }
+function Color(){
+  const classButton = btn.getAttribute("class");
+  btn.setAttribute("class", "light");
+  btn.innerText = "Lighten";
+  overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  if(classButton === "light"){
+    btn.setAttribute("class","dark");
+    btn.innerText = "Darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
   }
+}
